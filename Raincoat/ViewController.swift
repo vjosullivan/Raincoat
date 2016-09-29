c85d105e6c8d60926aa80be258ae4ec851116834
Vince O'Sullivan <vjosullivan@gmail.com>
2016-09-29 11:41:10 +0100

    DataPoint class fully definned an UT.

--- Raincoat/ViewController.swift
+++ Raincoat/ViewController.swift
@@ -12,21 +12,100 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var location: UILabel!
     
+    @IBOutlet weak var forecastTimestamp: UILabel!
+    @IBOutlet weak var sunTimes: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var apparentTemperature: UILabel!
+    @IBOutlet weak var maxTemperature: UILabel!
+    @IBOutlet weak var minTemperature: UILabel!
+    @IBOutlet weak var dewPoint: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var cloudLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var moonLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var iconLabel: UILabel!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let darkSky = DarkSkyClient(location: Location(latitude: 53.3, longitude: -1.0))
         darkSky.fetchForecast{ darkSkyForecast in
             print("Wahey! 4")
-            self.update(forecast: darkSkyForecast)
+            DispatchQueue.main.async {
+                self.update(forecast: darkSkyForecast)
+            }
         }
     }
     
     func update(forecast: DarkSkyForecast) {
-        print("Wahey! Updating screen for location \(forecast.latitude)N, \(forecast.longitude)W.")
-        print("Time: \(forecast.current.time).")
-        print("Temperature: \(forecast.current.temperature), (feels like \(forecast.current.apparentTemperature)).")
-        location.text = "Latitude: \(forecast.latitude)"
+        print("\n\n\(forecast)\n\n")
+        location.text = "\(abs(forecast.latitude.value))°\(forecast.latitude.value >= 0 ? "N" : "S"), \(abs(forecast.longitude.value))°\(forecast.longitude.value >= 0 ? "E" : "W")"
+        if let sunrise = forecast.current.sunriseTime {
+            sunTimes.text = "Sunrise ⋅ Sunset: \(local(time: sunrise, formattedAs: "HH:mm")) ⋅ \(local(time: forecast.current.sunsetTime!, formattedAs: "HH:mm"))"
+        } else {
+            sunTimes.text = "⋅"
+        }
+        forecastTimestamp.text = "Conditions at \(local(time: forecast.current.time, formattedAs: "HH:mm"))"
+        if let temp = forecast.current.temperature {
+            temperature.text = "Temperature: \(Int(temp.value))\(temp.unit.symbol)"
+        } else {
+            temperature.text = "⋅"
+        }
+        if let temp = forecast.current.apparentTemperature {
+            apparentTemperature.text = "Feels like : \(Int(temp.value))\(temp.unit.symbol)"
+        } else {
+            apparentTemperature.text = "⋅"
+        }
+        if let maxTemp = forecast.current.temperatureMax {
+            maxTemperature.text = "Max today: \(maxTemp)"
+        } else {
+            maxTemperature.text = "⋅"
+        }
+        if let minTemp = forecast.current.temperatureMin {
+            minTemperature.text = "Min today: \(minTemp)"
+        } else {
+            minTemperature.text = "⋅"
+        }
+        if let value = forecast.current.dewPoint {
+            dewPoint.text = "\(value.value >= 0 ? "Dew" : "Frost") point: \(value)"
+        } else {
+            dewPoint.text = "⋅"
+        }
+        if let value = forecast.current.pressure {
+            pressure.text = "Pressure: \(Int(value.value)) \(value.unit.symbol)"
+        } else {
+            pressure.text = "⋅"
+        }
+        if let value = forecast.current.humidity {
+            humidityLabel.text = "Humidity: \(Int(value * 100))%"
+        } else {
+            humidityLabel.text = "⋅"
+        }
+        cloudLabel.text = "Cloud cover: \(Int(forecast.current.cloudCover! * 100.0))%"
+        if let moonPhase = forecast.current.moonPhase {
+            moonLabel.text = "Moon: \(moonPhase)"
+        } else {
+            moonLabel.text = "⋅"
+        }
+        summaryLabel.text = forecast.current.summary!
+        windLabel.text = "Wind: \(forecast.current.windSpeed!) at \(forecast.current.windBearing!)"
+        if let precipType = forecast.current.precipType {
+            rainLabel.text = precipType
+        } else {
+            rainLabel.text = "No precipitation"
+        }
+        iconLabel.text = "Icon: \(forecast.current.icon!)"
+    }
+    
+    func local(time: Date, formattedAs: String) -> String {
+        // create dateFormatter with UTC time format
+        let formatter = DateFormatter()
+        formatter.dateFormat = formattedAs
+        formatter.timeZone = TimeZone.current
+        return formatter.string(from: time)
     }
 }
 
